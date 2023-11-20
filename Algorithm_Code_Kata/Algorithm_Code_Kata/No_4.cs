2384385a6c76e854498b47fd6bd96e675cbef830
leehs730 <leehs730@naver.com>
2023-11-20 09:23:18 +0900

    No_4
    나이 구하기

---
+++ Algorithm_Code_Kata/Algorithm_Code_Kata/No_4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Algorithm_Code_Kata
+{
+	public class No_4
+	{
+		public No_4()
+		{
+		}
+        public class Solution
+        {
+            public int solution(int age)
+            {
+                int answer = 0;
+                answer = 2023 - age;
+                return answer;
+            }
+        }
+    }
+}
+
---
+++ Algorithm_Code_Kata/Algorithm_Code_Kata/No_5.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Algorithm_Code_Kata
+{
+	public class No_5
+	{
+		public No_5()
+		{
+		}
+	}
+}
+
