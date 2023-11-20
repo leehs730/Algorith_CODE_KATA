e9a896b98c7ad6a91ac8ad6ebceffaa62c861ace
leehs730 <leehs730@naver.com>
2023-11-20 09:18:12 +0900

    No_2
    두 수의 곱

---
+++ Algorithm_Code_Kata/Algorithm_Code_Kata/No_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Algorithm_Code_Kata
+{
+	public class No_2
+	{
+		public No_2()
+		{
+		}
+		
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            answer = num1 * num2;
+            return answer;
+        }
+    }
+}
+}
+
