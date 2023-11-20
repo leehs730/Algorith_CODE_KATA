d202edba3a710a85ad97b926528530a6f4e3a0b5
leehs730 <leehs730@naver.com>
2023-11-20 09:25:21 +0900

    Update No_5.cs

--- Algorithm_Code_Kata/Algorithm_Code_Kata/No_5.cs
+++ Algorithm_Code_Kata/Algorithm_Code_Kata/No_5.cs
@@ -6,6 +6,16 @@ namespace Algorithm_Code_Kata
 		public No_5()
 		{
 		}
-	}
+
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = 0;
+                answer = (num1 == num2) ? 1 : -1;
+                return answer;
+            }
+        }
+    }
 }
 
