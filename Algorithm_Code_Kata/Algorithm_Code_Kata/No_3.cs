03c9a8f76ba892dc02dfb23d6ce76b7e00d421c0
leehs730 <leehs730@naver.com>
2023-11-20 09:20:24 +0900

    No_3
    몫 구하기

---
+++ Algorithm_Code_Kata/Algorithm_Code_Kata/No_3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Algorithm_Code_Kata
+{
+	public class No_3
+	{
+		public No_3()
+		{
+		}
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = 0;
+                answer = num1 / num2;
+                return answer;
+            }
+        }
+    }
+}
+
